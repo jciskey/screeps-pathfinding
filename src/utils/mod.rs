5eaffbeb637a41d79a4a5754d2ac6085d3fb1ea3
Joe Ciskey <jciskey@gmail.com>
2024-06-25 21:19:32 -0600

    Add helper methods

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,5 +1,6 @@
-use screeps::constants::Terrain;
+use screeps::constants::{Direction, Terrain};
 use screeps::local::{LocalCostMatrix, LocalRoomTerrain};
+use screeps::{Position, RoomXY};
 
 pub fn get_lcm_from_terrain(
     room_terrain: &LocalRoomTerrain,
@@ -17,4 +18,24 @@ pub fn get_lcm_from_terrain(
     }
 
     cm
+}
+
+pub fn room_xy_neighbors(node: RoomXY) -> Vec<RoomXY> {
+    node.neighbors()
+}
+
+pub fn position_neighbors(node: Position) -> Vec<Position> {
+    Direction::iter()
+        .filter_map(|dir| node.checked_add_direction(*dir).ok())
+        .collect()
+}
+
+pub fn movement_costs_from_lcm(lcm: &LocalCostMatrix) -> impl Fn(RoomXY) -> u32 + '_ {
+    |xy| {
+        let value = lcm.get(xy);
+        match value {
+            u8::MAX => u32::MAX,
+            _ => value as u32,
+        }
+    }
 }
