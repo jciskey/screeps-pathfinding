b993e9c8be9f94df2aa93f2e6a1183e3cad95d16
Joe Ciskey <jciskey@gmail.com>
2025-07-08 09:59:28 -0600

    Update A* implementation
    This splits A* into two separate sets of functions: generic A*, and
    grid-based A*. This allows us to implement grid-based optimizations,
    while also retaining a generic A* for graph-based searching.
    
    This also modifies the algorithms to have generic cost types, rather
    than being stuck using u32 as the cost.

--- src/utils/heuristics.rs
+++ src/utils/heuristics.rs
@@ -15,13 +15,12 @@ use crate::common::traits::GetRangeTo;
 ///
 /// let start = RoomXY::checked_new(24, 18).unwrap();
 /// let goal = RoomXY::checked_new(34, 40).unwrap();
-/// let cost_fn = |_| 1;
+/// let cost_fn = |_| Some(1);
 /// screeps_pathfinding::algorithms::astar::shortest_path_roomxy(
 ///     start,
 ///     &goal_exact_node(goal),
 ///     cost_fn,
 ///     &heuristic_get_range_to(goal),
-///     true,
 /// );
 pub fn heuristic_get_range_to<T: GetRangeTo + Copy + 'static>(goal: T) -> impl Fn(T) -> u32 {
     move |node: T| node.get_range_to(goal)
@@ -44,13 +43,12 @@ pub fn heuristic_get_range_to<T: GetRangeTo + Copy + 'static>(goal: T) -> impl F
 /// let goal_a = RoomXY::checked_new(34, 40).unwrap();
 /// let goal_b = RoomXY::checked_new(34, 45).unwrap();
 /// let goals = &[goal_a, goal_b];
-/// let cost_fn = |_| 1;
+/// let cost_fn = |_| Some(1);
 /// screeps_pathfinding::algorithms::astar::shortest_path_roomxy(
 ///     start,
 ///     &goal_exact_node_multigoal(goals),
 ///     cost_fn,
 ///     &heuristic_get_range_to_multigoal(goals),
-///     true,
 /// );
 pub fn heuristic_get_range_to_multigoal<T: GetRangeTo + Copy>(
     goals: &[T],
